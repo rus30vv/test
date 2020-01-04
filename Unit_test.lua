71a5b4229720ac11e0b9d84e8cc7388a4c8859ed
rus30vv <aes16bit@gmail.com>
2020-01-04 17:40:49 +0400

    first commit

--- Unit_test.lua
+++ Unit_test.lua
@@ -1,34 +1,35 @@
 Object = require "lib/classic-master/classic"
 --dofile("AnAL.lua")
 anim8 = require "anim8"
---Unit 
+--Unit
+--des git
 	Unit = Object:extend()
-	
+
 		Unit.x = 0
 		Unit.y = 35
 		Unit.jump = 0
 		Unit.pressed_mouse = 0
-		
-		
-		function Unit:load()		    
+
+
+		function Unit:load()
 		    player = love.graphics.newImage("pers.png")
-			
-  		end 
-		
-		
+
+  		end
+
+
 		function Unit:update(dt)
-            
+
 		    if love.mouse.isDown(1) then
 		    end
-				
+
 		    function love.keypressed(k)	then
 		    end
-		
+
 		end
 
-		
+
 		function Unit:draw()
-		--render backgroun, interactiv, mobs			
+		--render backgroun, interactiv, mobs
 		end
-		
+
 return Unit
